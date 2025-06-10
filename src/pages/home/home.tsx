b52ffc368c83f15813b299205318f57d36548c9d
cjq <cjq02@qq.com>
2025-06-11 00:03:50 +0800

    内容充实一些

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -6,6 +6,12 @@ import coverImg1 from '../../assets/img/hom-cover1jpg.jpg'
 import coverImg2 from '../../assets/img/home-cover2.jpg'
 import mainVideo from '../../assets/video/t_main_Android_demo_2x.mp4'
 
+// 导入新功能的图标
+import iconFast from '../../assets/img/icon_fast.svg'
+import iconSecure from '../../assets/img/icon_secure.svg'
+import iconPrivacy from '../../assets/img/icon_privacy.svg'
+import iconSync from '../../assets/img/icon_sync.svg'
+
 /**
  * 首页组件
  * @returns {React.ReactElement} 渲染的首页组件
@@ -68,6 +74,21 @@ function Home(): React.ReactElement {
               <p className="text-lg text-gray-700">连接世界每个角落</p>
             </div>
           </div>
+        </div>
+      </div>
+
+      {/* 行动呼吁部分 */}
+      <div className="cta-section py-20 bg-blue-600 text-white text-center">
+        <div className="container mx-auto px-4">
+          <h2 className="text-4xl font-bold mb-8">加入富业，开始您的数字旅程！</h2>
+          <a
+            href="https://im.fuye.io/app/index.php?i=1&c=entry&a=site&m=mdkeji_im&do=Index&state=Index"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block bg-white text-blue-600 text-xl font-semibold py-4 px-10 rounded-full shadow-lg hover:bg-gray-100 transition duration-300"
+          >
+            立即开始
+          </a>
         </div>
       </div>
 
