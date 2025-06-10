be1e0fbf662404bf58ae30055f523a62cd547fec
cjq <cjq02@qq.com>
2025-06-11 00:28:42 +0800

    图标

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
 import { Tabs } from 'antd-mobile'
-import { AppOutline, MessageOutline, UserOutline } from 'antd-mobile-icons'
 import './NavBar.css'
 
 /**
